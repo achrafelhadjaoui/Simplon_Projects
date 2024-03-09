7538edb0a1a800965763700ceac5b3177f9d2be9
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2024-03-10 00:12:43 +0100

    update

--- pret_immobilier.c
+++ pret_immobilier.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * La Société Chaabi Lil Iskane vous charge de développer un simulateur pour calculer le montant estimé d
+ * la mensualité d'un prêt immobilier en fonction: montant d'un prêt,
+ * taux d'intérêt, la durée de remboursement (an).  
+ * - après chaque exécution le programme redemande la saisie
+ * - La durée de moins de 5 ans n'est pas acceptée
+ * - le taux d'intérêt devrait être entre 3% et 7% (taux annuel)
+ * - Gérer un nombre maximum de tentatives: 10
+ */
+
+void simulateur(float montant_pret, float taux_interet, float montant_estime, int duree)
+{
+	taux_interet = (montant_pret * taux_interet) / 100;
+	montant_estime = taux_interet + montant_pret;
+	printf("le montant total d'un pret immobilier : %f\n", montant_estime);
+	duree = duree * 12;
+	montant_estime = montant_estime / duree;
+	printf("le montant estime de la mesualite d'un pret immobilier : %f\n", montant_estime);
+}
+
 int main()
 {
 	float montant_pret, taux_interet, montant_estime;
@@ -34,12 +54,7 @@ int main()
 			continue;
 		}
 
-		taux_interet = (montant_pret * taux_interet) / 100;
-		montant_estime = taux_interet + montant_pret;
-		printf("le montant total d'un pret immobilier : %f\n", montant_estime);
-		duree = duree * 12;
-		montant_estime = montant_estime / duree;
-		printf("le montant estime de la mesualite d'un pret immobilier : %f\n", montant_estime);
+		simulateur(montant_pret, taux_interet, montant_estime, duree);
 		break;
 	}
 	return 0;
